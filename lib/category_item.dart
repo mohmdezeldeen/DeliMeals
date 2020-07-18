6886189639fee9d5f60f53f1fab31e6cf1a09064
mohmdezeldeen <Mohamed.ezeldeen@gmail.com>
2020-07-18 23:19:41 +0200

    Initial commit

---
+++ lib/category_item.dart
@@ -0,0 +1,37 @@
+import 'package:deli_meals/category_meals_screen.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CategoryItem(this.title, this.color);
+
+  void _selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .push(MaterialPageRoute(builder: (_) => CategoryMealsScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            color.withOpacity(0.7),
+            color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
